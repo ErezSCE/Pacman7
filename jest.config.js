7657b0326848492e87c7f3b2e19d303a6b4e99b5
ErezKon <erezkon@gmail.com>
2026-08-09 15:30:23 +0300

    [pacman7]-[US-010]-fix: address senior-frontend review (iteration 1)

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
